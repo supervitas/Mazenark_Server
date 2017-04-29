9e3f6122807184ce56e66c5d1d9f8092ba239f86
VitalyNikolaev <nikolaev.vitally@yandex.ru>
2017-04-29 20:41:21 +0300

    pathes

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -6,14 +6,20 @@ import Rooms.RoomManager;
 public class main {
     public static void main(String[] args) {
         RoomManager roomManager = new RoomManager(20);
-
         port(9000);
-        get("/port", (req, res) -> {
-            Room active = roomManager.GetActiveRoom();
-            res.type("application/json");
 
-            return String.format("{\"port\":\"%s\"}", Integer.toString(active.getPort()));
+        path("/api", () -> {
+            before("/*", (q, a) -> a.type("application/json"));
+
+            get("/port", (req, res) -> {
+                Room active = roomManager.GetActiveRoom();
+
+                return String.format("{\"port\":\"%s\"}", Integer.toString(active.getPort()));
+            });
+
+            post("/gameresult", (req, res) -> "{\"status\":\"OK\"}");
         });
+
 
     }
 }
